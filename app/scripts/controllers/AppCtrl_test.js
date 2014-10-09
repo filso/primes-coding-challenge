d663e5feec885d431ab1ccdc3188f611e8a84cba
Filip Sobczak <filsob@gmail.com>
2014-10-09 01:52:51 +0100

    fix tests

--- app/scripts/controllers/AppCtrl_test.js
+++ app/scripts/controllers/AppCtrl_test.js
@@ -1,19 +1,50 @@
 describe('AppCtrl', function() {
   'use strict';
-  var scope, $httpBackend;
+  var scope, $httpBackend, iface;
 
   beforeEach(module('app'));
 
   beforeEach(inject(function($controller, $rootScope) {
 
+    iface = jasmine.createSpyObj('iface', ['message']);
     scope = $rootScope.$new();
-    $controller('TableCtrl', {
-      '$scope': scope
+
+    // to check broadcast event
+    spyOn(scope, '$broadcast');
+
+    $controller('AppCtrl', {
+      '$scope': scope,
+      'iface': iface
     });
 
   }));
 
-  it('should ', function() {
+  it('should initialise scope', inject(function(primesService) {
+    expect(primesService.primes).toBe(scope.primes);
+    expect(scope.inputNumber).toEqual(10);
+  }));
+
+  it('should handle user interaction', function() {
+
+    scope.$broadcast.reset();
+    scope.go();
+    expect(scope.$broadcast).toHaveBeenCalledWith('generateTable', { input : 10 });
+
+    scope.inputNumber = 30;
+    scope.$broadcast.reset();
+    scope.go();
+    expect(scope.$broadcast).toHaveBeenCalledWith('generateTable', { input : 30 });
+
+
+
+
+    scope.inputNumber = 60;
+    scope.$broadcast.reset();
+    scope.go();
+    expect(scope.$broadcast).not.toHaveBeenCalled();
+
+    // message - number to big
+    expect(iface.message).toHaveBeenCalledWith('The chosen number is too big - max 30');
 
   });
 
